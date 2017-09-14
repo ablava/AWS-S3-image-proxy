948903c4c8fc2f52a6ab8d55101dd745bdee6b0e
Alexandr Ablovatski <ablovatski@gmail.com>
2017-09-13 21:50:20 -0400

    Update make_html.sh

--- make_html.sh
+++ make_html.sh
@@ -14,13 +14,13 @@ if [ $# -eq 0 ]
     DATE=$1
 fi
 
-#Get the listing of images from S3 for that date
+# Get the listing of images from S3 for that date
 aws s3 ls s3://[BUCKET]/images/$DATE/jpg/ --no-paginate | tr -s ' ' | cut -d ' ' -f4 > images-$DATE.txt
 
-# count the number of images
+# Count the number of images
 images=`wc -l images-$DATE.txt | cut -f1 -d' '`
 
-#decide how many pages of 6 thumbs to create
+# Decide how many pages of 6 thumbs to create
 pages=$(expr `wc -l images-$DATE.txt | cut -f1 -d' '` / 6)
 
 # Need to add one more page if there is a division remainder
@@ -28,53 +28,57 @@ if [ "$(expr `wc -l images-$DATE.txt | cut -f1 -d' '` % 6)" -ne "0" ]; then
         pages=$(expr $pages + 1)
 fi
 
-#start the $DATE.html page
+# Start the $DATE.html page
 echo '<html><body>' > pages/$DATE.html
 
-# keep track of the current image in the listing
+# Keep track of the current image in the listing
 image_pos=1
 
-# start outer loop for each subpage
+# Start outer loop for each subpage
 i=1
 while [ $i -le $pages ]; do
 
-        # start the subpage
+        # Start the subpage
         outfile=pages/$DATE-$i.html
         echo '<html><body><div style="text-align:center;float:left">Camera snaps for '$DATE' - page '$i' out of '$pages'<br>'>$outfile
 
-        # exit inner loop when it reaches 6 iterations or the end of image listing
+        # Exit inner loop when it reaches 6 iterations or the end of image listing
         j=0
         listofpics=''
         while read pic; do
                 let j+=1
+                # Skip to next image until reaching the right one
                 if [ $j -lt $image_pos ]; then
                         continue
                 fi
+                # Stop when on the 6th image or at the end of the listing
                 if [ $j -ge $(($image_pos + 6)) ] || [ $j -gt $images ]; then
                         break
                 fi
                 
+                # Place the thumbnail on the subpage
                 echo '<a href="'../s3images/images/$DATE/jpg/$pic'"'' target="_blank"''><img src="'../s3images/resize/462x260/images/$DATE/jpg/$pic'" width="462" height="260"border="1" style="padding-top:2px;padding-bottom:2px;padding-left:2px;padding-right:2px;margin-top:5px;margin-bottom:5px;margin-left:5px;margin-right:5px;"></a>'
+
                 # Remember all the pictures on this subpage
                 listofpics="$listofpics $pic"
         done <<< "$(cat images-$DATE.txt)" >>$outfile
 
-        # advance the current image position by 6 and close the subpage
+        # Advance the current image position by 6 and close the subpage
         let image_pos+=6
         echo '</div></body></html>'>>$outfile
 
-        # add the link for this new subpage to the index file for the date
-        # include the list of images on that subpage (listofpics): 20170912-1.html: pic1, pic2
+        # Add the link for this new subpage to the index file for the date,
+        # include the list of thumbnails on that subpage (listofpics): 20170912-1.html: pic1, pic2
         echo '<a href="'$DATE-$i.html'">'$DATE-$i.html'</a>: '$listofpics'<br>' >> pages/$DATE.html
 
-        # increment page counter and return to create another subpage
+        # Increment page counter and return to create another subpage
         let i+=1
 done
 
-#close the $DATE.html page when done with all images
+# Close the $DATE.html page when done with all images
 echo '</body></html>' >> pages/$DATE.html
 
-# add the link for the date page to the index.html for the site
+# Add the link for the date page to the index.html for the site
 sed -i '$ d' index.html
 echo '<a href="pages/'$DATE.html'">'$DATE.html'</a><br>' >> index.html
 echo '</body></html>' >> index.html
